1d596c2e79264becf8135de4484c89593bbc31d8
Viktor_Zaluckij <zaluckii98@gmail.com>
2023-06-30 00:42:08 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,5 +3,13 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int result3 = func1(100);
+        System.out.println(result3);
+    }
+
+    public static int func1(int x) {
+        int y;
+        y = x * x;
+        return y;
     }
 }
