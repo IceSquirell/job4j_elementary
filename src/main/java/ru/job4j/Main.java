22001ae5cc96b311de9431fcbeb8e0915d5e1886
Viktor_Zaluckij <zaluckii98@gmail.com>
2023-07-01 17:36:39 +0300

    4.2. Результат работы метода [#232622 #426574]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,15 +1,11 @@
 package ru.job4j;
 
+import ru.job4j.calculator.MathFunc;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        int result3 = func1(100);
+        int result3 = MathFunc.func1(100);
         System.out.println(result3);
-    }
-
-    public static int func1(int x) {
-        int y;
-        y = x * x;
-        return y;
     }
 }
---
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+public class MathFunc {
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+}
+
