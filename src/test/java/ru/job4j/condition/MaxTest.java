a94c363045af45ef5cb5f758eef45abb92299284
Viktor_Zaluckij <zaluckii98@gmail.com>
2023-07-11 23:08:04 +0300

    4.2. Тернарное сравнение [#189 #428877]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -13,4 +13,22 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax1To2Then1() {
+        int left = 2;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenOneIsEqualsTwo() {
+        int left = 2;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
 }
