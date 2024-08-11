42d80f6dbd184a08660437d618b0fed8e0f0f051
Viktor_Zaluckij <zaluckii98@gmail.com>
2024-08-11 22:19:36 +0300

    6.3. Массив заполнен true или false [#53857 #507724]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,6 +2,14 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
-        return postfix[postfix.length - 1] == word[word.length - 1];
+        int wordIndex = word.length - postfix.length;
+        for (char c : postfix) {
+            if (c == word[wordIndex]) {
+                wordIndex++;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
